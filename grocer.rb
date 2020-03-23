b500c251b231dd9de40c1a1351d24b479c419d9e
Kathryn Clark <kathrynlael@gmail.com>
2020-03-23 21:20:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -66,7 +66,12 @@ def apply_coupons(cart, coupons)
        cart_item_with_coupon[:count] += coupons[i][:num]
        cart_item[:count] -= coupons[i][:num]
    else 
-     cart_ca
+     cart_item_with_coupon = {
+       :item => couponed_item_name, 
+       :price => coupons[i][:cost] / coupons[i][:num],
+       :count => coupons[i][:num],
+       :clearance => cart_item[:clearance]
+       } 
        cart << cart_item_with_coupon
        cart_item[:count] -= coupons[i][:num]
      end
